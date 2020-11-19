6be50580b4c6138782d095dfe92ffa6748b72c82
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-11-19 18:21:38 -0500

    Skeleton descriptive analysis

---
+++ training/descriptive.py
@@ -0,0 +1,62 @@
+# %%
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+
+from training.utils import get_img_metadata, get_dx_labels
+
+img_metadata = get_img_metadata()
+dx_lbls = get_dx_labels()
+
+x = img_metadata[dx_lbls]
+
+k = 15
+kmeans = KMeans(n_clusters=k)
+
+
+# %%
+def plot_data(X):
+    plt.plot(X[:, 0], X[:, 1], 'k.', markersize=2)
+
+
+def plot_centroids(centroids, weights=None, circle_color='w', cross_color='k'):
+    if weights is not None:
+        centroids = centroids[weights > weights.max() / 10]
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='o', s=30, linewidths=8,
+                color=circle_color, zorder=10, alpha=0.9)
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='x', s=50, linewidths=50,
+                color=cross_color, zorder=11, alpha=1)
+
+
+def plot_decision_boundaries(clusterer, X, resolution=1000, show_centroids=True,
+                             show_xlabels=True, show_ylabels=True):
+    mins = X.min(axis=0) - 0.1
+    maxs = X.max(axis=0) + 0.1
+    xx, yy = np.meshgrid(np.linspace(mins[0], maxs[0], resolution),
+                         np.linspace(mins[1], maxs[1], resolution))
+    Z = clusterer.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.contourf(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
+                 cmap="Pastel2")
+    plt.contour(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
+                linewidths=1, colors='k')
+    plot_data(X)
+    if show_centroids:
+        plot_centroids(clusterer.cluster_centers_)
+
+    if show_xlabels:
+        plt.xlabel("$x_1$", fontsize=14)
+    else:
+        plt.tick_params(labelbottom=False)
+    if show_ylabels:
+        plt.ylabel("$x_2$", fontsize=14, rotation=0)
+    else:
+        plt.tick_params(labelleft=False)
+
+
+# %%
+kmeans.fit(x)
+plot_decision_boundaries(kmeans, x)
