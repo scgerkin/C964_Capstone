8be749c6ea7c8c3ad8d126bd69c6e3a6857a7471
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-11-25 15:53:45 -0500

    Set up label prediction portion.
    Label predictions work as expected, and returning the probabilities
    also works with the tensorflow/serving container running.

--- ml/api/server.py
+++ ml/api/server.py
@@ -1,6 +1,8 @@
+import json
+import requests
 import os
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
+import numpy as np
 from numpy import expand_dims
 from flask import Flask
 from flask_restful import Api, Resource, abort
@@ -14,7 +16,7 @@ api = Api(app)
 
 # todo: get bucket name from environ or config file
 # todo: add bucket policy to automatically delete after set time
-BUCKET_NAME = "xray.scgrk.com"
+BUCKET_NAME = "scgrk.com"
 
 # TODO: set model path. Keep in container or get from s3?
 PROJECT_DIR = "W:/WGU/C964_Capstone/project/ml/"
@@ -22,25 +24,24 @@ PROJECT_DIR = "W:/WGU/C964_Capstone/project/ml/"
 finding_predictor_path = "models/20201124-155102-100-binary-kmeans.pkl"
 finding_predictor_path = os.path.join(PROJECT_DIR, finding_predictor_path)
 
-label_classifier_path = "models/20201123-120422_dx-classifier-final.h5"
-label_classifier_path = os.path.join(PROJECT_DIR, label_classifier_path)
 
-labels = ["atelectasis", "cardiomegaly", "consolidation", "edema", "effusion",
-          "emphysema", "fibrosis", "hernia", "infiltration", "mass", "nodule",
-          "pleural_thickening", "pneumonia", "pneumothorax"]
-
-
-def load_models():
+def load_finding_model():
     with open(finding_predictor_path, 'rb') as f:
         f_predictor = pickle.load(f)
-    l_classifier = load_model(label_classifier_path)
-    return f_predictor, l_classifier
+    return f_predictor
 
 
-finding_predictor, label_classifier = load_models()
+finding_predictor = load_finding_model()
 
 
 class Bucket(Resource):
+    """
+    FIXME: I think I might replace this with just posting the image directly
+        to the server, saving it in a temp folder, and processing it from
+        that rather than deal with S3. Since `load_img` can't load from a web
+        URL file path, putting it onto S3 is just an extra step that adds no
+        benefit.
+    """
     s3 = boto3.client("s3")
 
     expiration = 600  # 10 minutes
@@ -59,30 +60,57 @@ class Bucket(Resource):
 
 
 def load_img_as_array(filename):
-    # TODO: This will (probably?) not work without making the bucket public
-    #  Lazy solution is to make gets public.
-    #  Easy/inefficient is to get presigned url
-    #  Correct is to use the s3 client, download it locally (if unable to load
-    #   directly from memory), then load from the saved file
-    filepath = f"https://s3.amazonaws.com/{BUCKET_NAME}/{filename}"
+    # FIXME: Apparently load_img can't take a web URL
+    # filepath = f"https://s3.amazonaws.com/{BUCKET_NAME}/{filename}"
+    filepath = "W:/WGU/C964_Capstone/project/ml/dataset/images/00000001_000.png"
     img = load_img(filepath, target_size=(256, 256), color_mode='grayscale')
     img = img_to_array(img)
     return img
 
 
 def make_predictions(img):
-    finding_prediction = finding_predictor.predict(img.flatten())
-    tensor = expand_dims(img, axis=0)
-    label_prediction = label_classifier.predict(tensor)
+    finding_label = get_finding_prediction(img)
+    labelled_predictions = get_label_prediction(img)
+    return {"Finding": finding_label, "Label": labelled_predictions}
 
+
+def get_finding_prediction(img):
+    img = img.flatten()
+    # FIXME: above is not flattening from 2d to 1d array, so predictor
+    #  has a fit
+    # finding_prediction = finding_predictor.predict(img)
+
+    finding_prediction = 1
     # if prediction is 0, then there is a finding
-    finding_label = True if finding_prediction < 0.5 else False
+    return finding_prediction < 0.5
 
-    label_probs = {}
-    for i in range(len(label_prediction)):
-        label_probs[labels[i]] = label_prediction[i]
 
-    return {"Finding": finding_label, "Label": label_probs}
+def get_label_prediction(img):
+    tensor = expand_dims(img, axis=0)
+
+    classifier_request = json.dumps({
+        "signature_name": "serving_default",
+        "instances"     : tensor.tolist()
+        })
+
+    server_url = "http://localhost:8501/v1/models/dx-classifier:predict"
+    response = requests.post(server_url, data=classifier_request)
+    response = response.json()
+
+    predictions = np.array(response["predictions"][0])
+    predictions.round(2)
+    predictions = predictions.tolist()
+
+    labels = ["atelectasis", "cardiomegaly", "consolidation", "edema",
+              "effusion", "emphysema", "fibrosis", "hernia", "infiltration",
+              "mass", "nodule", "pleural_thickening", "pneumonia",
+              "pneumothorax"]
+
+    labelled_predictions = {}
+    for i in range(len(predictions)):
+        labelled_predictions[labels[i]] = predictions[i]
+
+    return labelled_predictions
 
 
 class Predictor(Resource):
@@ -91,6 +119,9 @@ class Predictor(Resource):
         img = load_img_as_array(filename)
         predictions = make_predictions(img)
         return predictions
+
+    def post(self):
+        pass
 
 
 api.add_resource(Bucket, "/upload")
