703ff0b18a3cffa9cd4c4f3e40997e86e9b8fb43
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-11-29 14:00:02 -0500

    Refactor chart display to segregate functionality

--- presentation/src/components/analysis/resultsChart.js
+++ presentation/src/components/analysis/resultsChart.js
@@ -7,97 +7,117 @@ const TRANSITION_DURATION = 500 // ms
 
 class ResultsChart {
   constructor(element, data) {
-    const vis = this
-
-    vis.svg = d3.select(element)
-                .append("svg")
-                .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-                .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
-                .append("g")
-                .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
-
-    vis.xLabel = vis.svg.append("text")
-                    .attr("x", WIDTH / 2)
-                    .attr("y", HEIGHT + 50)
-                    .attr("text-anchor", "middle")
-                    .text("Diagnostic Labels")
-
-    vis.yLabel = vis.svg.append("text")
-                    .attr("x", HEIGHT / -2)
-                    .attr("y", -50)
-                    .attr("text-anchor", "middle")
-                    .text("Probability %")
-                    .attr("transform", "rotate(-90)")
-
-    vis.xAxisGroup = vis.svg.append("g")
-                        .attr("transform", `translate(0, ${HEIGHT})`)
-
-    vis.yAxisGroup = vis.svg.append("g")
-
+    this.initSVG(element)
+    this.initXYLabels()
+    this.initAxesGroups()
     this.update(data)
   }
 
+  initSVG(element) {
+    this.svg = d3.select(element)
+                 .append("svg")
+                 .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+                 .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+                 .append("g")
+                 .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+  }
+
+  initXYLabels() {
+    this.xLabel = this.svg.append("text")
+                      .attr("x", WIDTH / 2)
+                      .attr("y", HEIGHT + 50)
+                      .attr("text-anchor", "middle")
+                      .text("Diagnostic Labels")
+
+    this.yLabel = this.svg.append("text")
+                      .attr("x", HEIGHT / -2)
+                      .attr("y", -50)
+                      .attr("text-anchor", "middle")
+                      .text("Probability %")
+                      .attr("transform", "rotate(-90)")
+
+  }
+
+  initAxesGroups() {
+    this.xAxisGroup = this.svg.append("g")
+                          .attr("transform", `translate(0, ${HEIGHT})`)
+
+    this.yAxisGroup = this.svg.append("g")
+  }
+
   update(data) {
-    const vis = this
+    this.data = data
+    this.setXY()
+    this.setAxes()
+    this.joinData()
+    this.removeStaleRects()
+    this.updateExistingRects()
+    this.addNewRects()
+  }
 
-    vis.data = data
-    console.log(data)
+  setXY() {
+    this.y = d3.scaleLinear()
+               .domain([0, 1])
+               .range([HEIGHT, 0])
 
-    const y = d3.scaleLinear()
-                .domain([0, 1])
-                .range([HEIGHT, 0])
+    this.x = d3.scaleBand()
+               .domain(this.data.labels.map(i => i.label))
+               .range([0, WIDTH])
+               .padding(0.4)
 
-    const x = d3.scaleBand()
-                .domain(vis.data.labels.map(i => i.label))
-                .range([0, WIDTH])
-                .padding(0.4)
+  }
 
-    const xAxisCall = d3.axisBottom(x)
-    vis.xAxisGroup.transition()
-       .duration(TRANSITION_DURATION)
-       .call(xAxisCall)
+  setAxes() {
+    const xAxisCall = d3.axisBottom(this.x)
+    this.xAxisGroup.transition()
+        .duration(TRANSITION_DURATION)
+        .call(xAxisCall)
 
-    const yAxisCall = d3.axisLeft(y)
-    vis.yAxisGroup.transition()
-       .duration(TRANSITION_DURATION)
-       .call(yAxisCall)
+    const yAxisCall = d3.axisLeft(this.y)
+    this.yAxisGroup.transition()
+        .duration(TRANSITION_DURATION)
+        .call(yAxisCall)
+  }
 
-    // JOIN
-    const rects = vis.svg.selectAll("rect")
-                     .data(vis.data.labels)
+  joinData() {
+    this.rects = this.svg.selectAll("rect")
+                     .data(this.data.labels)
+  }
 
-    // EXIT
-    rects.exit()
-         .transition()
-         .duration(TRANSITION_DURATION)
-         .attr("height", 0)
-         .attr("y", HEIGHT)
-         .remove()
+  removeStaleRects() {
+    this.rects.exit()
+        .transition()
+        .duration(TRANSITION_DURATION)
+        .attr("height", 0)
+        .attr("y", HEIGHT)
+        .remove()
+  }
 
-    // UPDATE
-    rects.transition()
-         .duration(TRANSITION_DURATION)
-         .attr("x", d => x(d.label))
-         .attr("y", d => y(d.probability))
-         .attr("width", x.bandwidth)
-         .attr("height", d => HEIGHT - y(d.height))
+  updateExistingRects() {
+    this.rects.transition()
+        .duration(TRANSITION_DURATION)
+        .attr("x", d => this.x(d.label))
+        .attr("y", d => this.y(d.probability))
+        .attr("width", this.x.bandwidth)
+        .attr("height", d => HEIGHT - this.y(d.height))
+  }
 
-    // ENTER
-    rects.enter()
-         .append("rect")
-         .attr("x", d => x(d.label))
-         .attr("width", x.bandwidth())
-         .attr("fill", d => {
-           if (d.probability > 0.5) {
-             return "red"
-           }
-           return "grey"
-         })
-         .attr("y", HEIGHT)
-         .transition()
-         .duration(TRANSITION_DURATION)
-         .attr("height", d => HEIGHT - y(d.probability))
-         .attr("y", d => y(d.probability))
+  addNewRects() {
+    this.rects.enter()
+        .append("rect")
+        .attr("x", d => this.x(d.label))
+        .attr("width", this.x.bandwidth())
+        .attr("fill", d => {
+          if (d.probability > 0.5) {
+            return "red"
+          }
+          return "grey"
+        })
+        .attr("y", HEIGHT)
+        .transition()
+        .duration(TRANSITION_DURATION)
+        .attr("height", d => HEIGHT - this.y(d.probability))
+        .attr("y", d => this.y(d.probability))
   }
 }
 
