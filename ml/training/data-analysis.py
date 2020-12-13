8cae81c37a4f9f6ab7c6e29f03480a312b17c1d5
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-12-13 11:25:33 -0500

    Add graphs for image classification

--- ml/training/data-analysis.py
+++ ml/training/data-analysis.py
@@ -1,17 +1,74 @@
 # %%
+from copy import deepcopy
 import pandas as pd
-from training.utils import get_img_metadata, get_dx_labels
+from training.utils import get_img_metadata, get_dx_labels, PROJECT_DIR
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import seaborn as sns
 
 plt.style.use("seaborn-poster")
+sns.set_style("darkgrid")
+sns.set_theme(palette="Set2")
+
+
+def save_show(fig_name):
+    path = f"{PROJECT_DIR}analysis/data/{fig_name}.png"
+    plt.savefig(path)
+    plt.show()
+
 
 trimmed = pd.read_csv("training/train_data.csv")
 original = get_img_metadata()
 dx_labels = get_dx_labels()
-dx_counts = {}
-for label in dx_labels:
-    dx_counts[label] = len(original[original[label] > 0.5])
+training_dx_labels = deepcopy(dx_labels)
+training_dx_labels.remove("hernia")
+training_dx_labels.remove("pneumonia")
+
 # %%
-plt.figure(figsize=(20, 10))
-plt.barh(dx_labels, list(dx_counts.values()), align="center")
-plt.show()
+images_by_set = pd.melt(trimmed, id_vars=["split_set"],
+                        value_vars=training_dx_labels,
+                        var_name="Classification", value_name="Count")
+images_by_set = images_by_set.loc[images_by_set.Count > 0]
+# %%
+plt.figure(figsize=(21, 10))
+sns.countplot(y="Classification", hue="split_set", data=images_by_set,
+              order=images_by_set["Classification"].value_counts().index)
+plt.legend(bbox_to_anchor=(1.0, 0), loc=2, borderaxespad=0.0)
+plt.title("Image classification by usage split")
+plt.xlabel("Count")
+save_show("classification-by-split")
+# %%
+only_dx_by_sex = pd.melt(original.drop(["no_finding"], axis=1),
+                         id_vars=["pt_sex"],
+                         value_vars=[lbl for lbl in dx_labels if
+                                     lbl != "no_finding"],
+                         var_name="Classification",
+                         value_name="Count")
+only_dx_by_sex = only_dx_by_sex.loc[only_dx_by_sex.Count > 0]
+
+plt.figure(figsize=(14, 8))
+gs = gridspec.GridSpec(14, 1)
+ax1 = plt.subplot(gs[:12, :])
+ax2 = plt.subplot(gs[13, :])
+
+sns.countplot(y="Classification",
+              hue="pt_sex",
+              data=only_dx_by_sex,
+              order=only_dx_by_sex["Classification"].value_counts().index,
+              ax=ax1)
+ax1.set(xlabel="", ylabel="Classification")
+ax1.set_title("Image Classification by patient sex")
+only_no_finding_by_sex = pd.melt(original[["pt_sex", "no_finding"]],
+                                 id_vars=["pt_sex"],
+                                 value_vars=["no_finding"],
+                                 var_name="No Finding", value_name="Count")
+only_no_finding_by_sex = only_no_finding_by_sex.loc[
+    only_no_finding_by_sex.Count > 0]
+sns.countplot(y="No Finding",
+              hue="pt_sex",
+              data=only_no_finding_by_sex,
+              order=only_no_finding_by_sex["No Finding"].value_counts().index,
+              ax=ax2)
+ax2.legend("")
+ax2.set(xlabel="Count", ylabel="")
+save_show("classification-by-sex")
