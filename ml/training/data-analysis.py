064bb650abceabfb7936f40fabaf048ddac2c044
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-12-13 13:55:27 -0500

    Add dx by age freq graphs

--- ml/training/data-analysis.py
+++ ml/training/data-analysis.py
@@ -29,7 +29,7 @@ images_by_set = pd.melt(trimmed, id_vars=["split_set"],
                         value_vars=training_dx_labels,
                         var_name="Classification", value_name="Count")
 images_by_set = images_by_set.loc[images_by_set.Count > 0]
-# %%
+
 plt.figure(figsize=(21, 10))
 sns.countplot(y="Classification", hue="split_set", data=images_by_set,
               order=images_by_set["Classification"].value_counts().index)
@@ -72,3 +72,45 @@ sns.countplot(y="No Finding",
 ax2.legend("")
 ax2.set(xlabel="Count", ylabel="")
 save_show("classification-by-sex")
+
+# %%
+
+markers = ["o", "v", "^", "<", ">", "1", "2", "3", "4", "*", "x", "p", "h", "D"]
+fig, ax = plt.subplots(figsize=(8, 8))
+lbls = [lbl for lbl in dx_labels if lbl != "no_finding"]
+for label, marker in zip(lbls, markers):
+    counts = original[original[label] > 0]["pt_age"].value_counts()
+    sns.scatterplot(x=counts.index,
+                    y=counts.values,
+                    label=label,
+                    marker=marker,
+                    alpha=1,
+                    s=70, ax=ax)
+plt.title("Diagnostic Frequency by Age")
+plt.xlabel("Age")
+plt.xticks(ticks=range(0, 100, 10))
+plt.ylabel("Frequency")
+save_show("dx-freq-by-age")
+# %%
+row = col = 0
+fig, axes = plt.subplots(7, 2, sharex="all", figsize=(8, 9))
+
+for label, marker in zip(lbls, markers):
+    ax = axes[row, col]
+    ax.set_title(label)
+    ax.set_xticks(range(0, 100, 10))
+
+    counts = original[original[label] > 0]["pt_age"].value_counts()
+    sns.scatterplot(x=counts.index,
+                    y=counts.values,
+                    marker=marker,
+                    alpha=1,
+                    s=35, ax=ax)
+    col = (col + 1) % 2
+    if col == 0:
+        row = (row + 1) % 7
+fig.subplots_adjust(hspace=0.6)
+fig.suptitle("Individual Diagnostic Frequencies by Age")
+fig.text(0.5, 0.04, "Age", ha="center", va="center")
+fig.text(0.05, 0.5, "Frequency", ha="center", va="center", rotation="vertical")
+save_show("indiv-dx-by-age")
