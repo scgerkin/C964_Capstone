78b2480a195851ef721859109a520cf903974c6e
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-12-14 15:52:58 -0500

    Get project ready for submission

--- ml/mvfiles.py
+++ ml/mvfiles.py
@@ -1,15 +1,14 @@
 import shutil
 import os
 
-test_files = [i for i in open("test_files.txt").read().split(",")]
-valid_files = [i for i in open("valid_files.txt").read().split(",")]
-train_files = [i for i in open("train_files.txt").read().split(",")]
+test_files = [i for i in open("analysis/test_files.txt").read().split("\n")]
+valid_files = [i for i in open("analysis/valid_files.txt").read().split("\n")]
+train_files = [i for i in open("analysis/train_files.txt").read().split("\n")]
 
 origin = "W:\\WGU\\C964_Capstone\\project\\ml\\dataset\\images"
-target = "W:\\tmp\\mltrain"
+target = "W:\\tmp\\project"
 
 for item in test_files + valid_files + train_files:
-  item = item[:-1] if item[len(item)-1:] == "\n" else item
   from_loc = os.path.join(origin, item)
   to_loc = os.path.join(target, item)
   shutil.copyfile(from_loc, to_loc)
